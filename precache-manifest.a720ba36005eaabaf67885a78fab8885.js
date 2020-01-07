98053ada78aa3f5a7b747cf8e361324018f745b6
Gil Atari <gil.atari@gmail.com>
2020-01-07 16:23:26 +0200

    Updates

---
+++ precache-manifest.a720ba36005eaabaf67885a78fab8885.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1d28b8f28868cc28af5d30aede520d5",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "099fd3131991a98cb23d",
+    "url": "/weather/static/css/main.4bf481c4.chunk.css"
+  },
+  {
+    "revision": "35050770fb1b3374256a",
+    "url": "/weather/static/js/2.978c95d4.chunk.js"
+  },
+  {
+    "revision": "78ddf5a465591a03b9601ba2ddd63415",
+    "url": "/weather/static/js/2.978c95d4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "099fd3131991a98cb23d",
+    "url": "/weather/static/js/main.a9e28c37.chunk.js"
+  },
+  {
+    "revision": "c451136a3d6c63544975",
+    "url": "/weather/static/js/runtime-main.243164e3.js"
+  }
+]);
